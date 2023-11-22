3cd6d397b4168c8ec247c0877c742300da918f99
guoyi <1900013009@pku.edu.cn>
2023-11-22 19:35:58 +0800

    update

--- todo.sh
+++ todo.sh
@@ -1,22 +1,19 @@
-python train.py --cfg ./configs/defalut.json --split_name 1
-# python train.py --cfg ./configs/defalut.json --split_name 2
-python train.py --cfg ./configs/defalut.json --split_name 3
-python train.py --cfg ./configs/defalut.json --split_name 4
+python train.py --cfg ./configs/efficientnet_b7.json --split_name 1 
+python train.py --cfg ./configs/efficientnet_b7.json --split_name 2 
+python train.py --cfg ./configs/efficientnet_b7.json --split_name 3 
+python train.py --cfg ./configs/efficientnet_b7.json --split_name 4 
 
-python train.py --cfg ./configs/resnet50.json --split_name 1
-python train.py --cfg ./configs/resnet50.json --split_name 2
-python train.py --cfg ./configs/resnet50.json --split_name 3
-python train.py --cfg ./configs/resnet50.json --split_name 4
+python train.py --cfg ./configs/resnet18.json --split_name 1 
+python train.py --cfg ./configs/resnet18.json --split_name 2 
+python train.py --cfg ./configs/resnet18.json --split_name 3 
+python train.py --cfg ./configs/resnet18.json --split_name 4 
 
-python train.py --cfg ./configs/resnet18.json --split_name 1
-python train.py --cfg ./configs/resnet18.json --split_name 2
-python train.py --cfg ./configs/resnet18.json --split_name 3
-python train.py --cfg ./configs/resnet18.json --split_name 4
+python train.py --cfg ./configs/mobelnetv3_small.json --split_name 1 
+python train.py --cfg ./configs/mobelnetv3_small.json --split_name 2 
+python train.py --cfg ./configs/mobelnetv3_small.json --split_name 3 
+python train.py --cfg ./configs/mobelnetv3_small.json --split_name 4 
 
-python train.py --cfg ./configs/resnet18.json --split_name 1
-python train.py --cfg ./configs/resnet18.json --split_name 2
-python train.py --cfg ./configs/resnet18.json --split_name 3
-python train.py --cfg ./configs/resnet18.json --split_name 4
-
-python ring.py
-shutdown
+python train.py --cfg ./configs/inceptionv3.json --split_name 1 --resize 299
+python train.py --cfg ./configs/inceptionv3.json --split_name 2 --resize 299
+python train.py --cfg ./configs/inceptionv3.json --split_name 3 --resize 299
+python train.py --cfg ./configs/inceptionv3.json --split_name 4 --resize 299
